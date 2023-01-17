5641a77c31f19c5bc570e00bb345c291cd595cbc
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-01-17 14:30:45 +0100

    Create MWPZ06X - Nowa działka.cpp

---
+++ MWPZ06X - Nowa działka.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int x, D;
+
+int main()
+{
+    cin >> D;
+    for (int i = 0; i < D; i++)
+    {
+        cin >> x;
+        cout << x * x << endl;
+    }
+    return 0;
+}
